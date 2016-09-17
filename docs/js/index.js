57bb7a54a53d3149c0e7c4790334713aaf1deef3
Andrew Grossfeld <grossfeld.andrew@gmail.com>
2016-09-17 16:11:50 -0400

    new stylish header on scroll

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
+    var pre_pop = true;
+    var header_init_pos = true;
+    if (pre_pop) { $(".popover-wrapper").addClass("show"); pre_pop = false; }
+
+    /*--------------------Event_Handlers--------------------*/
     $("#close").on("click", function() {
         $(".popover-wrapper").removeClass("show");
-        $(".popover-wrapper").addClass("hide");
+    });
+
+    $(".scroll-back").on("click", function() {
+        document.body.scrollTop = 0;
+        return false;
+    });
+
+    $(window).on("scroll", function() {
+        var scroll_target = 180;
+        var scroll_pos = document.body.scrollTop;
+        if (header_init_pos && scroll_pos > scroll_target) {
+            $(".header").addClass("min");
+            header_init_pos = false;
+        }
+        else if (!header_init_pos && scroll_pos <= scroll_target) {
+            $(".header").removeClass("min");
+            header_init_pos = true;
+        }
+
     });
 });
