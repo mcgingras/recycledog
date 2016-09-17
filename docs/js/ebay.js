b55c6a6c88716e7b609de049d551a8fd75b2da64
ncskier <bcw58@cornell.edu>
2016-09-17 13:34:43 -0400

    parsing url - scraping url 24

--- docs/js/ebay.js
+++ docs/js/ebay.js
@@ -79,8 +79,8 @@ function run_ebay_query(query_str_lst) {
       url += "&RESPONSE-DATA-FORMAT=JSON";
       url += "&callback=_cb_findItemsByKeywords";
       url += "&REST-PAYLOAD";
-      url += keywords;
-      url += "&paginationInput.entriesPerPage=3";
+      url += "&keywords=harry%20potter";
+      url += "&paginationInput.entriesPerPage=5";
 
 
   // Submit the request
