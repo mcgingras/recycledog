801257da9cada66e926afe30bd262061a2356a6c
ncskier <bcw58@cornell.edu>
2016-09-17 10:31:14 -0400

    parsing url - log url

--- docs/js/parse.js
+++ docs/js/parse.js
@@ -3,15 +3,14 @@ function get(name) {
     return decodeURIComponent(name[1]);
 }
 
-var url = get('url');
-
 function getPosition(str, m, i) {
    return str.split(m, i).join(m).length;
 }
 
+var url = get('url');
 var exp = url.split('.');
 var store = exp[1];
 
-alert('store is' + store);
-
-alert("url: " + url);
+console.log('url: ' + url);
+console.log('explode: ' + exp);
+console.log('store is ' + store);
