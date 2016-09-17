a389813c2312a78cff2164009abad8af46a36c78
M <mcg79@cornell.edu>
2016-09-17 19:42:11 -0400

    adding titles

--- docs/js/ebay.js
+++ docs/js/ebay.js
@@ -14,10 +14,10 @@ function _cb_findItemsByKeywords(root) {
 
 
     var item     = items[i];
-    console.log(item.galleryURL);
     var title    = item.title;
     var price    = item.currentPrice;
     var pic      = item.galleryURL.toString().replace('http:','https:');
+    console.log("pic value is" +pic +"and has type"+typeOf(pic));
     var viewitem = item.viewItemURL;
     if (null != title && null != viewitem) {
       html.push('<div class="grid"><div class="grid--img" style="background-image: url("'+pic+'")"></div><div class="grid--info"><h4>'+title+'</h4></div></div>');
