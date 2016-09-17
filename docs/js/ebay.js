fab91c2e5e7f4b806e7ba6b3b46c88d2e7565733
ncskier <bcw58@cornell.edu>
2016-09-17 13:43:28 -0400

    query ebay with url input 2

--- docs/js/ebay.js
+++ docs/js/ebay.js
@@ -79,7 +79,7 @@ function run_ebay_query(query_str_lst) {
       url += "&RESPONSE-DATA-FORMAT=JSON";
       url += "&callback=_cb_findItemsByKeywords";
       url += "&REST-PAYLOAD";
-      url += "&keywords=jcrew%20mens_category";
+      url += "&keywords=jcrew";
       url += "&paginationInput.entriesPerPage=5";
 
 
