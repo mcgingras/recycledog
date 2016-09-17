f0f9a4ff4be4b920796b6b0c85547be3dcc8f0b2
ncskier <bcw58@cornell.edu>
2016-09-17 12:26:19 -0400

    parsing url - scraping url 6

--- docs/js/parse.js
+++ docs/js/parse.js
@@ -39,7 +39,7 @@ console.log('query_str_lst: ' + query_str_lst);
 // QUERY Yahoo Query Language to get website data
 var yql_query = 'SELECT * FROM html WHERE url="'+url+'"';
 
-var query_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fstackoverflow.com%22&format=json&diagnostics=true&callback="
+var query_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.jcrew.com%2Fmens_category%2Fsweaters%2FJCrewcashmere%2FPRDOVR~F3994%2FF3994.jsp%22&format=json&diagnostics=true&callback="
 
 var title_List = $.getJSON(query_url);
 var response_json = title_list.resonseJSON.query.results.body;
