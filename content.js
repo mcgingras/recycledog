4f7498dad213a955bfe787d5ce44a7771f79c40b
M <mcg79@cornell.edu>
2016-09-16 22:27:31 -0400

    small changes curtosy of brandon thx dog

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('window.html'), 'selected': true});
+    chrome.tabs.create({'url': ('https://mcgingras.github.io/bigredhacks/'), 'selected': true});
 });
 
 chrome.extension.onMessage.addListener(
@@ -9,10 +9,8 @@ chrome.extension.onMessage.addListener(
   });
 
 chrome.tabs.getSelected(null, function(tab) {
-
   // Now inject a script onto the page
   chrome.tabs.executeScript(tab.id, {
        code: "chrome.extension.sendMessage({content: document.body.innerHTML}, function(response) { console.log('success'); });"
      }, function() { console.log('done'); });
-
 });
