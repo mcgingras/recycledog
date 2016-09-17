bfc8bc3a9ec86c468c985fff459aa2326668aa02
ncskier <bcw58@cornell.edu>
2016-09-17 11:28:57 -0400

    parsing url - creating query list 2

--- docs/js/parse.js
+++ docs/js/parse.js
@@ -5,20 +5,27 @@ function get(name) {
 
 var query_str_lst = [];
 var url = get('url');
-var exp = url.split('.');
 var str_array = url.split('/');
-var store = exp[1];
+
+// Get store name from website name
+var exp = url.split('.');
+var store_name = exp[1];
 
 console.log('url: ' + url);
 console.log('explode: ' + exp);
-console.log('store is ' + store);
+console.log('store is ' + store_name);
 console.log('str_array: ' + str_array);
 
 for (var i = 0; i < str_array.length; i++) {
-  // trim website name to (hopefully) company name
   var str = str_array[i];
   console.log('element: ' + str);
 
+  // replace www.company_name.com with just company_name
+  if (str.includes('www.'+store_name)) {
+    str_array[i] = store_name
+  }
+
+  // remove empty elements
   if (str != "") {
     query_str_lst.push(str);
   }
