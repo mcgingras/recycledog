7109113e5ac139a0f2e85445b17fb41ec716cb3b
Andrew Grossfeld <grossfeld.andrew@gmail.com>
2016-09-17 16:24:50 -0400

    refactor morph text

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,7 +3,23 @@ $(document).ready(function() {
     var header_init_pos = true;
     if (pre_pop) { $(".popover-wrapper").addClass("show"); pre_pop = false; }
 
+
+    /*--------------------Rotating_Tag--------------------*/
+
+    $("#js-rotating").Morphext({
+        // The [in] animation type. Refer to Animate.css for a list of available animations.
+        animation: "flipInX",
+        // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+        separator: ",",
+        // The delay between the changing of each phrase in milliseconds.
+        speed: 5000,
+        complete: function () {
+            // Called after the entrance animation is executed.
+        }
+    });
+
     /*--------------------Event_Handlers--------------------*/
+    
     $("#close").on("click", function() {
         $(".popover-wrapper").removeClass("show");
     });
